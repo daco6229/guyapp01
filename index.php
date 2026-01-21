f3eb5644b078fd335f917e9348070bfe0ee76013
daco6229 <guy.plankeele@gmail.com>
2026-01-21 16:12:04 +0100

    Create index.php
    Add index.php

---
+++ index.php
@@ -0,0 +1,8 @@
+<?php
+echo "<h1>Le lundi on est BLEU.</h1>";
+echo "<h2>Le mardi on est BLEU.</h2>";
+echo "<h2>Le mercredi on est BLEU.</h2>";
+echo "<h2>Le jeudi on est BLEU.</h2>";
+echo "<h2>Le vendredi… ON EST BLEU.</h2>";
+echo "<p>PHP: " . PHP_VERSION . "</p>";
+?>
